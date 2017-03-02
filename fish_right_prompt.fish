616c95902eee6ae246b5fa48115b32054786a11b
Grant Wu <grantwu2@gmail.com>
2017-03-02 13:22:28 -0800

    Fix double-space issue in right prompt
    Was showing two spaces b/w branch and timestamp if repo wasn't dirty and
    branches were in sync

--- fish_right_prompt.fish
+++ fish_right_prompt.fish
@@ -28,15 +28,17 @@ function fish_right_prompt
 
     echo -n -s $repository_color " " (git_branch_name) $normal_color " "
 
+    echo -n -s (git_ahead $ahead $behind $diverged)
+
     if git_is_touched
       echo -n -s $dirty
+      echo -n -s " "
     end
-      echo -n -s (git_ahead $ahead $behind $diverged $none)
+
   else
     echo -n -s $directory_color $cwd $normal_color
+    echo -n -s " "
   end
-
-  echo -n -s " "
 
   set_color $fish_color_autosuggestion ^/dev/null; or set_color 555
   date "+%H:%M"
